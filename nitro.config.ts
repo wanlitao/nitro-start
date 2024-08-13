b500e9084b4520f785d2e0652718af17b2f42e38
万里涛 <wanlitao_3055601@outlook.com>
2024-08-13 09:52:48 +0800

    adjust project directories

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,9 +1,12 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   compatibilityDate: '2024-08-07',
-  srcDir: "server",
+  srcDir: "src/backend",
+  output: {
+    dir: "dist/backend"
+  },
   experimental: {
     openAPI: true,
     asyncContext: true
-  }  
+  }
 })
