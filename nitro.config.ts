0cd8ec794a6da725c2bf5d620498784e1a8571a1
万里涛 <wanlitao_3055601@outlook.com>
2024-08-27 20:33:39 +0800

    add build linux target config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,12 +1,12 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-  compatibilityDate: '2024-08-07',
   srcDir: "src/backend",
   output: {
-    dir: "dist/backend"
-  },
+    dir: "dist/backend",
+  },  
   experimental: {
     openAPI: true,
-    asyncContext: true
-  }  
-})
+    asyncContext: true,
+  },
+  compatibilityDate: "2024-08-27",
+});
